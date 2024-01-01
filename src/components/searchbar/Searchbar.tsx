d146d34f216986d7d6aeb9d8ef5a256b4e3fd45d
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-01 21:55:57 +0100

    Add search icon and more styles to Searchbar component, add drop-shadow variable for light theme

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,20 +1,23 @@
 import RegularButton from "../buttons/RegularButton";
+import SearchSvg from "../icons/SearchSvg";
 import "./Searchbar.css";
 
 function Searchbar() {
-    return (
-        <section className="searchbar">
-            <p>o</p>
-            <input 
-                type="text" 
-                className="searchbar-input" 
-                placeholder="Search GitHub username..."
-            />
-            <div className="searchbar-button">
-                <RegularButton buttonText={"Search"} />
-            </div>
-        </section>
-    );
+  return (
+    <section className="searchbar">
+      <div className="search-icon">
+        <SearchSvg />
+      </div>
+      <input
+        type="text"
+        className="searchbar-input"
+        placeholder="Search GitHub username..."
+      />
+      <div className="searchbar-button">
+        <RegularButton buttonText={"Search"} />
+      </div>
+    </section>
+  );
 }
 
 export default Searchbar;
