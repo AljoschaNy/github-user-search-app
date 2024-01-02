0cb92985581bbf951c45c844b41801d013dab082
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-02 23:19:50 +0100

    Add props to UserStats component

--- src/components/main/UserStats.tsx
+++ src/components/main/UserStats.tsx
@@ -1,19 +1,20 @@
+import { UserStatsProps } from "../../type/types";
 import "./UserStats.css";
 
-function UserStats() {
+function UserStats({ repoCount, totalFollowers, totalFollowing }: Readonly<UserStatsProps>) {
   return (
     <section className="user-stats">
       <div className="user-stat">
         <h4>Repos</h4>
-        <p>8</p>
+        <p>{repoCount}</p>
       </div>
       <div className="user-stat">
         <h4>Followers</h4>
-        <p>856</p>
+        <p>{totalFollowers}</p>
       </div>
       <div className="user-stat">
         <h4>Following</h4>
-        <p>28</p>
+        <p>{totalFollowing}</p>
       </div>
     </section>
   );
--- src/type/types.ts
+++ src/type/types.ts
@@ -7,3 +7,9 @@ export type RegularButtonProps = {
   buttonText: string,
   onClick?: () => void
 }
+
+export type UserStatsProps = {
+  repoCount: number,
+  totalFollowers: number,
+  totalFollowing: number
+}
