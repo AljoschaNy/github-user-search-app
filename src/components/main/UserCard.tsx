f382b5eae2dd868ae138a6451731c143448e365f
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-03 18:29:08 +0100

    Add components to UserCard component and refactor code, add some styling

--- src/components/main/UserCard.tsx
+++ src/components/main/UserCard.tsx
@@ -1,43 +1,51 @@
+import MoreInfos from "./MoreInfos";
+import "./UserCard.css";
+import UserStats from "./UserStats";
+
 function UserCard() {
-    return (
-        <div className="user-card">
-            <img className="desktop-view-element"/>
-            <section className="user-card-info">
-                <div className="user-card-header">
-                    <img className="mobile-view-element"/>
-                    <div className="user-card-name">
-                        <div>
-                            <h2>The Octocat</h2>
-                            <p>@octocat</p>
-                        </div>
-                        <p className="joined-date">Joined 25 Jan 2011</p>
-                    </div>
-                    
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</p>
-                <section className="user-stats">
-                    <div className="user-stat">
-                        <p>Repos</p>
-                        <p>8</p>
-                    </div>
-                    <div className="user-stat">
-                        <p>Followers</p>
-                        <p>856</p>
-                    </div>
-                    <div className="user-stat">
-                        <p>Following</p>
-                        <p>28</p>
-                    </div>
-                </section>
-                <ul>
-                    <li>San Francisco</li>
-                    <li>Not Available</li>
-                    <li>https://github.blog</li>
-                    <li>@github</li>
-                </ul>
-            </section>
+  const userBio = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.";
+
+  return (
+    <div className="user-card">
+      <img
+        className="desktop-view-element"
+        src={"image-src"}
+        alt="profile picture"
+      />
+
+      <section className="user-card-info">
+        <div className="user-card-header">
+          <img className="mobile-view-element" src="" alt="profile picture" />
+
+          <div className="user-card-name">
+            <div>
+              <h1>{"The Octocat"}</h1>
+              <a className="github-link" href="#">
+                {"@octocat"}
+              </a>
+              <p className="joined-date mobile-view-element">
+                {"Joined 25 Jan 2011"}
+              </p>
+            </div>
+            <p className="joined-date desktop-view-element">
+              Joined 25 Jan 2011
+            </p>
+          </div>
         </div>
-    );
+        {userBio 
+          ? <p className="user-bio">{userBio}</p>
+          : <p className="user-bio not-available">This profile has no bio</p>
+        }
+        <UserStats repoCount={8} totalFollowers={25} totalFollowing={20} />
+        <MoreInfos
+          location={"San Francisco"}
+          companyLink={"@github"}
+          // twitterLink={"twitter-link"}
+          // websiteLink={"website-link"}
+        />
+      </section>
+    </div>
+  );
 }
 
 export default UserCard;
